b79ab21a86aefcd621564559b55d984d2dbce92d
avazjon7 <avazjon867@gmail.com>
2024-08-03 21:33:55 +0500

    twelveth project

---
+++ online_shop/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+
+from online_shop.models import Product, Category, Comment, Order
+
+# Register your models here.
+
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Comment)
+admin.site.register(Order)
